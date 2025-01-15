4eda46edb7fbfbd45c216a52277be6fe8a1cd43e
Nile Jocson <nile.oblivia@gmail.com>
2025-01-15 14:38:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# shibal
+
+A scheduling and budgeting utility.
---
+++ src/main.cxx
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: Copyright (C) Nile Jocson <nile.oblivia@gmail.com>
+// SPDX-License-Identifier: MPL-2.0
+
+
+
+#include <cstdlib>
+
+#include <iostream>
+
+
+
+auto main() -> int
+{
+	std::cout << "Hello, world!\n";
+
+	return EXIT_SUCCESS;
+}
