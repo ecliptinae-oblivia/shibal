b3295552b3d092cb124cda45ee9bc3cef1e0b671
Nile Jocson <nile.oblivia@gmail.com>
2025-01-16 00:25:06 +0800

    Test ImGui

--- src/ecliptinae-oblivia/shibal/main.cxx
+++ src/ecliptinae-oblivia/shibal/main.cxx
@@ -45,16 +45,48 @@ auto main() -> int
 
 
 
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	[[maybe_unused]] auto &io = ImGui::GetIO();
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 460");
+
+
+
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.6314f, 0.8902f, 0.8235f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		auto show = true;
+		ImGui::ShowDemoWindow(&show);
+
+		ImGui::Render();
+
+		auto width = 0;
+		auto height = 0;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	}
 
 
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	glfwTerminate();
 
